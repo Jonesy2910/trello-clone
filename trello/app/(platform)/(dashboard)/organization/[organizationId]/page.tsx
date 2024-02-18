dddb88ca282d4aaf6adb318a347e386bdad78c13
Dan_J <dan_jones1107@hotmail.com>
2024-02-18 19:23:24 +0000

    Adding Board to Prisma Database

--- trello/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,28 @@
+import {db} from "@/lib/db";
 
 const OrganizationIdPage = () => {
 
+    async function create(formData: FormData) {
+        "use server";
+
+        const title =formData.get("title") as string;
+        await db.board.create({
+            data: {
+                title,
+            }
+        })
+    }
+
     return (
         <div>
-            Organisation Page
+            <form action={create}>
+                <input
+                id={"title"}
+                name={"title"}
+                required
+                placeholder={"Enter a board title"}
+                className={"border-black border p-1 "}/>
+            </form>
         </div>
     )
 }
