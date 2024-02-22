ff056ae8979f55fea37650270cec9da7a5203ac2
Dan_J <dan_jones1107@hotmail.com>
2024-02-22 22:41:52 +0000

    Adding Lists to Database to be stored.

--- trello/app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ trello/app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,6 +1,7 @@
 
 import {Board} from "@prisma/client";
 import {BoardTitleForm} from "@/app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form";
+import {BoardOptions} from "@/app/(platform)/(dashboard)/board/[boardId]/_components/board-options";
 
 interface BoardNavbarProps {
     data: Board;
@@ -13,6 +14,10 @@ export const BoardNavbar = async ({
     return (
         <div className={"w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white"}>
             <BoardTitleForm data={data} />
+            <div className={"ml-auto"}>
+                <BoardOptions id={data.id}/>
+            </div>
         </div>
+
     )
 }
